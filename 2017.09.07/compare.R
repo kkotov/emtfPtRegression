1e835abc66e1d00a4cc41b15b5b9964d3343f6e1
kkotov <kkotov@cern.ch>
2017-09-08 16:48:03 +0200

    shifting row.names to the end

--- 2017.09.07/compare.R
+++ 2017.09.07/compare.R
@@ -2,7 +2,7 @@ require(ggplot2)
 source("../metrics.R")
 
 if( !file.exists("three.csv") ){
-  system("echo \"muPtGen\",\"theta\",\"st1_ring2\",\"dPhi12\",\"dPhi13\",\"dPhi14\",\"dPhi23\",\"dPhi24\",\"dPhi34\",\"dTheta12\",\"dTheta13\",\"dTheta14\",\"dTheta23\",\"dTheta24\",\"dTheta34\",\"dPhiS4\",\"dPhiS4A\",\"dPhiS3\",\"dPhiS3A\",\"clct1\",\"clct2\",\"clct3\",\"clct4\",\"fr1\",\"fr2\",\"fr3\",\"fr4\",\"rpc1\",\"rpc2\",\"rpc3\",\"rpc4\",\"ptXML\",\"myPt\",\"row.names\" > three.csv")
+  system("echo \"muPtGenInv\",\"theta\",\"st1_ring2\",\"dPhi12\",\"dPhi13\",\"dPhi14\",\"dPhi23\",\"dPhi24\",\"dPhi34\",\"dTheta12\",\"dTheta13\",\"dTheta14\",\"dTheta23\",\"dTheta24\",\"dTheta34\",\"dPhiS4\",\"dPhiS4A\",\"dPhiS3\",\"dPhiS3A\",\"clct1\",\"clct2\",\"clct3\",\"clct4\",\"fr1\",\"fr2\",\"fr3\",\"fr4\",\"rpc1\",\"rpc2\",\"rpc3\",\"rpc4\",\"ptXML\",\"train\",\"ranger\",\"myPt\",\"row.names\" > three.csv")
   system("cat two.csv | sed -e 's|(int)||g' -e 's|(float)||g' -e 's| $||g' -e 's| |,|g' >> three.csv")
 }
 
@@ -10,9 +10,9 @@ rateShapeBinned = c(1,1/seq(2,200,0.5)^3)
 binning = seq(2,200,0.5)
 nBins <- length(binning)
 
-df <- read.csv("three.csv",sep=',',header=T,row.names=34)
+df <- read.csv("three.csv",sep=',',header=T,row.names=36)
 
-testSet <- data.frame(res = exp(df$myPt), muPtGen = exp(df$muPtGen), ptTrg = exp(df$ptXML))
+testSet <- data.frame(res = 1./df$myPt, muPtGen = 1./df$muPtGenInv, ptTrg = df$ptXML)
 testSet$trueBin <- sapply(testSet$muPtGen, findBin, binning)
 pSpec <- aggregate(testSet, by=list(bin=testSet$trueBin), function(x) x )
 pSpec$count <- sapply(pSpec$res,length)
